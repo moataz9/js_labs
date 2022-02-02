c042ca79553caed571b3a8e5ffa61422a058f978
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-02 15:42:33 +0200

    standrad deviation - fundamentals

---
+++ 06-fundamental/level_4/175-standard-deviation.js
@@ -0,0 +1,15 @@
+/**
+ * get the standard deviation of an array of numbers.
+ */
+
+function standardDeviation(arr, usePopulation = false) {
+  const mean = arr.reduce((acc, val) => acc + val, 0) / arr.length
+
+  return Math.sqrt(
+    arr.reduce((acc, val) => acc.concat((val - mean) ** 2), []).reduce((acc, val) => acc + val, 0) /
+      (arr.length - (usePopulation ? 0 : 1))
+  )
+}
+
+console.log(standardDeviation([10, 2, 38, 23, 38, 23, 21]))
+console.log(standardDeviation([10, 2, 38, 23, 38, 23, 21], true))
