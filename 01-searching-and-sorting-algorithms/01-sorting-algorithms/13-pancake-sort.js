021f28d1cc66988f4a185b90141e857ac3745eb8
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-04 06:30:30 +0200

    pancake-sort algorithm

---
+++ 01-searching-and-sorting-algorithms/01-sorting-algorithms/13-pancake-sort.js
@@ -0,0 +1,31 @@
+function pancake_sort(arr) {
+  for (let i = arr.length - 1; i >= 1; i--) {
+    // find the index of the largest element not yet sorted
+    let max_idx = 0
+    let max = arr[0]
+    for (let j = 1; j <= i; j++) {
+      if (arr[j] > max) {
+        max = arr[j]
+        max_idx = j
+      }
+    }
+
+    if (max_idx == i) continue // element already in place
+
+    let new_slice
+
+    // flip arr max element to index 0
+    if (max_idx > 0) {
+      new_slice = arr.slice(0, max_idx + 1).reverse()
+      for (j = 0; j <= max_idx; j++) arr[j] = new_slice[j]
+    }
+
+    // then flip the max element to its place
+    new_slice = arr.slice(0, i + 1).reverse()
+    for (j = 0; j <= i; j++) arr[j] = new_slice[j]
+  }
+  return arr
+}
+
+console.log('Original Array Elements:\n', [3, 0, 2, 5, -1, 4, 1])
+console.log('Sorted Array Elements:\n', pancake_sort([3, 0, 2, 5, -1, 4, 1]))
