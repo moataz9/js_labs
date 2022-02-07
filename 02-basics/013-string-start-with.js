05bffe7ce509571e29b2f671815eeb1544d499c1
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-07 03:13:14 +0200

    string starts with

---
+++ 02-basics/013-string-start-with.js
@@ -0,0 +1,9 @@
+/**
+ * check whether a string starts with spacific character/s and false otherwise.
+ */
+
+function str_start_with(str, start_with) {
+  return str.startsWith(start_with);
+}
+
+console.log(str_start_with('moataz samir', 'mo'));
