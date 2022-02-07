08e6e804284b8d2fa97ce57dddf74bd8756b191b
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-07 07:13:42 +0200

    near 100

---
+++ 02-basics/015-near-100.js
@@ -0,0 +1,17 @@
+function near_100(x, y) {
+  if (x != y) {
+    x1 = Math.abs(x - 100)
+    y1 = Math.abs(y - 100)
+
+    if (x1 < y1) return x
+
+    if (y1 < x1) return y
+
+    return 0
+  } else return false
+}
+
+console.log(near_100(90, 89))
+console.log(near_100(102, 103))
+console.log(near_100(-90, -89))
+console.log(near_100(90, 90))
