624f1762ccc90da83f53eba4c5d016efdbc73081
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-10 13:31:41 +0200

    delete item - Object

---
+++ 03-object/02-delete-item.js
@@ -0,0 +1,13 @@
+/**
+ * delete the rollno property from the following object. Also print the object before or after deleting the property.
+ */
+
+let student = {
+  name: 'David Rayy',
+  sclass: 'VI',
+  rollno: 12,
+}
+
+console.log(student)
+delete student.rollno
+console.log(student)
