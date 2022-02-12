b7ff5346685f188e91d2510e024efc3034fb1831
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-12 02:20:34 +0200

    substrings - Function

---
+++ 05-function/03-substrings.js
@@ -0,0 +1,27 @@
+/**
+ * generates all combinations of a string.
+ */
+function substrings(str1) {
+  let array1 = []
+  for (let x = 0, y = 1; x < str1.length; x++, y++) {
+    array1[x] = str1.substring(x, y)
+  }
+  let combi = []
+  let temp = ''
+  let slent = Math.pow(2, array1.length)
+
+  for (let i = 0; i < slent; i++) {
+    temp = ''
+    for (let j = 0; j < array1.length; j++) {
+      if (i & Math.pow(2, j)) {
+        temp += array1[j]
+      }
+    }
+    if (temp !== '') {
+      combi.push(temp)
+    }
+  }
+  console.log(combi.join(', '))
+}
+
+substrings('dog')
