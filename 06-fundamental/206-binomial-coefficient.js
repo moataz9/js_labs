6f8c952de870762cbbca61a1f42089c791abd252
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-14 08:11:26 +0200

    binomial cofficient - fundamentals

---
+++ 06-fundamental/206-binomial-coefficient.js
@@ -0,0 +1,16 @@
+/**
+ * Evaluate the binomial coefficient of two integers n and k
+ */
+
+function binomialCoefficient(n, k) {
+  if (Number.isNaN(n) || Number.isNaN(k)) return NaN
+  if (k < 0 || k > n) return 0
+  if (k === 0 || k === n) return 1
+  if (k === 1 || k === n - 1) return n
+  if (n - k < k) k = n - k
+  let res = n
+  for (let j = 2; j <= k; j++) res *= (n - j + 1) / j
+  return Math.round(res)
+}
+
+console.log(binomialCoefficient(8, 2))
