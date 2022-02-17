43539e8d18e56d3868f5d008a9bda8482cac8a9a
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-17 02:11:15 +0200

    to safe integer - fundamentals

---
+++ 06-fundamental/level_1/27-to-safe-integer.js
@@ -0,0 +1,11 @@
+/**
+ * convert a value to a safe integer.
+ */
+
+function toSafeInteger(num) {
+  return Math.round(Math.max(Math.min(num, Number.MAX_SAFE_INTEGER), Number.MIN_SAFE_INTEGER))
+}
+
+console.log(toSafeInteger('5.2'))
+console.log(toSafeInteger('5.52'))
+console.log(toSafeInteger(Infinity))
