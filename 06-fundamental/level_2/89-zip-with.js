ed2772ce379194f4afa99c43f9c7c8286614bf24
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-02-21 10:20:54 +0200

    zip with - fundamentals

---
+++ 06-fundamental/level_2/89-zip-with.js
@@ -0,0 +1,24 @@
+/**
+ * Create an array of elements,
+ * grouped based on the position in the original arrays and using function
+ * as the last value to specify how grouped values should be combined
+ */
+
+function zipWith(...array) {
+  const fn = typeof array[array.length - 1] === 'function' ? array.pop() : undefined
+
+  return Array.from({ length: Math.max(...array.map(a => a.length)) }, (_, i) =>
+    fn ? fn(...array.map(a => a[i])) : array.map(a => a[i])
+  )
+}
+
+console.log(zipWith([1, 2], [10, 20], [100, 200], (a, b, c) => a + b + c)) // [111,222]
+
+console.log(
+  zipWith(
+    [1, 2, 3],
+    [10, 20],
+    [100, 200],
+    (a, b, c) => (a != null ? a : 'a') + (b != null ? b : 'b') + (c != null ? c : 'c')
+  )
+)
